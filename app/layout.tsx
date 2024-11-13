902987a139a12b516ef1def8f096329c372e8835
Jake Steelman <33525082+jakesteelman@users.noreply.github.com>
2024-11-13 17:55:00 -0600

    final clean up pass

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
                     disableTransitionOnChange
                 >
                     <main className="min-h-screen flex flex-col items-center">
-                        <div className="flex-1 w-full flex flex-col gap-20 items-center">
+                        <div className="flex-1 w-full flex flex-col gap-8 items-stretch">
                             <NavigationBar />
                             <div className="flex flex-col gap-20 max-w-5xl p-5">
                                 {children}
--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = ({
     ...props
 }: Props) => {
     return (
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16" {...props}>
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-6" {...props}>
             <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                 <p>
                     © {new Date().getFullYear()} Macroscanner. All rights reserved.
