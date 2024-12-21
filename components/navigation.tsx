b0c3db25847d0d3f058074079fe79ca125115dbc
Jake Steelman <33525082+jakesteelman@users.noreply.github.com>
2024-12-21 12:46:05 -0600

    fix logo

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,6 +5,7 @@ import HeaderAuth from "@/components/header-auth";
 import { buttonVariants } from './ui/button';
 import { Crown } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { Badge } from './ui/badge';
 
 type Props = {} & React.HTMLAttributes<HTMLDivElement>
 
@@ -22,8 +23,9 @@ const NavigationBar = ({
     return (
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16" {...props}>
             <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                <Link href="/">
-                    <Logo className='h-8' />
+                <Link href="/" className='flex items-center gap-2'>
+                    <Logo className='h-7' />
+                    <Badge variant='outline' className='text-xs'>BETA</Badge>
                 </Link>
                 <div className='flex gap-4 items-center justify-end'>
                     <Link href="/pro" className={cn('text-yellow-600 dark:text-yellow-500', buttonVariants({ variant: 'ghost', size: 'sm' }))}>
