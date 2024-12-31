dfd49c7d2263b2ec2c7c5d3a555f64f21ac589e3
Jake Steelman <33525082+jakesteelman@users.noreply.github.com>
2024-12-30 18:51:06 -0600

    add SVG icon and update layout metadata for improved SEO and viewport settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,41 @@ import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import localFont from 'next/font/local';
-const defaultUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
+import type { Metadata, Viewport } from 'next'
+
+const defaultUrl = process.env.NEXT_PUBLIC_SITE_URL
+    ? `${process.env.NEXT_PUBLIC_SITE_URL}`
     : "http://localhost:3000";
 
-
-export const metadata = {
+export const metadata: Metadata = {
     metadataBase: new URL(defaultUrl),
     title: "Macroscanner | AI-Powered Food Photo Journal",
     description: "Effortless Food Intelligence. Macroscanner is an AI-powered food photo journal that helps you track your diet and nutrition.",
+    applicationName: "Macroscanner",
+    openGraph: {
+        title: "Macroscanner | AI-Powered Food Photo Journal",
+        description: "Effortless Food Intelligence. Macroscanner is an AI-powered food photo journal that helps you track your diet and nutrition.",
+        url: defaultUrl,
+        siteName: "Macroscanner",
+        images: [
+            {
+                url: `${defaultUrl}/og-image.webp`,
+                width: 1200,
+                height: 630,
+                alt: "An image showing Macroscanner's logo with the text AI Powered Food Nutrition Scanner.",
+            }
+        ],
+        locale: "en_US",
+        type: 'website'
+    },
 };
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false
+}
 
 const font = localFont({
     src: './AspektaVF.woff2',
