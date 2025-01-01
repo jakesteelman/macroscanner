8e5fbe4be780e0a62841981705f8b0e1408e1183
Jake Steelman <33525082+jakesteelman@users.noreply.github.com>
2025-01-01 17:36:23 -0600

    set trial end date for users without it during OAuth session exchange

--- app/(auth)/auth/callback/route.ts
+++ app/(auth)/auth/callback/route.ts
@@ -8,9 +8,21 @@ export async function GET(request: Request) {
 
     if (code) {
         const supabase = await createClient()
-        const { error } = await supabase.auth.exchangeCodeForSession(code)
-        console.error('auth error!', error)
+        const { data, error } = await supabase.auth.exchangeCodeForSession(code)
+
         if (!error) {
+
+            if (!data.user.user_metadata.trial_end_utc) {
+                console.log('user does not have trial end date set, setting it now')
+                // Supabase doesn't let you add data to the user_metadata if a user uses oauth. so we have do to it here
+                const updateUserResult = await supabase.auth.updateUser({
+                    data: {
+                        trial_end_utc: new Date().toISOString()
+                    }
+                })
+                console.log('updateUserResult', updateUserResult)
+            }
+
             const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
             const isLocalEnv = process.env.NODE_ENV === 'development'
             if (isLocalEnv) {
